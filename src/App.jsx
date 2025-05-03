c96a06889b40436202bcdaea87f3dda4f1ff65b2
Aadityadav <aadit.211602@ncit.edu.np>
2025-05-03 14:43:20 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+       
+        <a href="https://aadityadav.netlify.app" target="_blank">
+          <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/ChatGPT-Logo.svg/640px-ChatGPT-Logo.svg.png' className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>ChatGPT 2.O is Comming </h1>
+      <div className="card">
+        <p>
+          A new version of AI model . Stay tuned for updates!
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Aadit Yadav - Developer
+      </p>
+    </>
+  )
+}
+
+export default App
